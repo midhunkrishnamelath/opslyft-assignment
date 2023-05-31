4715a05abb444542ca29d31607489fa86fcf55ee
Midhun Krishna <100683432+midhunkrishnamelath@users.noreply.github.com>
2023-06-01 05:14:04 +0530

    Update app.js

--- exampleapp/app.js
+++ exampleapp/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Define an API endpoint
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.json({ message: 'Hello from the exampleapp API!' });
 });
 
