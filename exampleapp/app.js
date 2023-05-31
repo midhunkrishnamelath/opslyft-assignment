63dbdf7c5130d918f7f0b5bfb3da8f23ffb8c8e3
midhunkrishnamelath@gmail.com <midhunkrishnamelath@gmail.com>
2023-06-01 00:41:02 +0530

    app commit for ci/cd test

---
+++ exampleapp/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define an API endpoint
+app.get('/exampleapp/', (req, res) => {
+  res.json({ message: 'Hello from the exampleapp API!' });
+});
+
+// Start the server
+const port = 3002;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
